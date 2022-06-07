97d4d2a55282a7574045f86569b2fabc87865d93
ayrhkhan <ayrhkhan@gmail.com>
2022-06-07 15:06:27 -0500

    Studio 15

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,51 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import static java.util.Objects.isNull;
+
 public class User {
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+    @Email(regexp = ".+@.+\\..+||null", message = "Email is invalid")
     private String email;
+    @NotBlank(message ="Password is required")
+    @Size(min =6, message = "Password at least 6 characters long" )
     private String password;
+//    @NotBlank(message = "Verify is required")
+    @NotNull(message = "Passwords do not match")
+    private String verify;
+
+
+
+    private static int nextId = 1;
+
+    private int id;
 
     public User() {
-
+        this.id = nextId;
+        nextId++;
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+        checkPassword();
     }
 
     public String getUsername() {
@@ -38,5 +70,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    private void checkPassword(){
+        if(!isNull(this.password) && !isNull(this.verify) && !this.password.equals(this.verify)){
+            this.verify = null;
+        }
+
     }
 }
